4655d5e942cebed111dc20fda09dd6413a14d297
istoeaquilo <fabianomachado@gmail.com>
2025-06-14 12:59:06 -0300

    Add files via upload

---
+++ admin/src/firebaseConfig.js
@@ -0,0 +1,16 @@
+// admin/src/firebaseConfig.js
+
+// IMPORTANTE: Substitua os valores abaixo pelos da sua conta do Firebase.
+// Esta é a maneira mais robusta de garantir que a sua aplicação se conecta corretamente.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcaqKBWMOX3vIrdzi5AirPsOKxuJcHryo",
+  authDomain: "fidelidade-isto-e-aquilo.firebaseapp.com",
+  projectId: "fidelidade-isto-e-aquilo",
+  storageBucket: "fidelidade-isto-e-aquilo.firebasestorage.app",
+  messagingSenderId: "347388047322",
+  appId: "1:347388047322:web:94c959a19f6b81b75bdae3"
+};
+
+export const appId = "fidelidade-isto-e-aquilo";
+export const initialAuthToken = null;
